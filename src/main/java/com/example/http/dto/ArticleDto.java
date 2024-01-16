ea40203ef805ecd89ddbd9e0b9abca14f2941fdc
Jang2723 <jangsb46@gmail.com>
2024-01-16 15:47:24 +0900

    01/16 HTTP

---
+++ src/main/java/com/example/http/dto/ArticleDto.java
@@ -0,0 +1,26 @@
+package com.example.http.dto;
+
+import lombok.Data;
+
+// 사용자가 작성하고 싶은 게시글 데이터를 의미한다.
+// 실제 데이터
+// 제목 : 직렬화 역직렬화
+// 내용 : Serialization
+/*
+// JSON으로 표현된 데이터
+{
+    "title": "직렬화 역직렬화",
+    "content": "Serialization"
+}
+
+// Java에서 실제로 사용하는 객체
+ArticleDto dto = new ArticleDto();
+dto.setTitle("직렬화 역직렬화");
+dto.setContent("Serialization");
+
+*/
+@Data
+public class ArticleDto {
+    private String title;
+    private String content;
+}
